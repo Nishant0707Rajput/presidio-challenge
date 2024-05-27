1e1d1715e47ca1337674376b5551041495be19c4
nishant.rajput <nishant.rajput@appventurez.com>
2024-05-27 23:16:41 +0530

    removed sigterm

--- index.js
+++ index.js
@@ -9,29 +9,29 @@ const morgan = require('morgan');
 const app = express();
 app.use(morgan('dev'));
 app.use(cors());
-app.use((req, res, next) => {
-  // Website you wish to allow to connect
-  res.setHeader("Access-Control-Allow-Origin", "*");
+// app.use((req, res, next) => {
+//   // Website you wish to allow to connect
+//   res.setHeader("Access-Control-Allow-Origin", "*");
 
-  // Request methods you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, PATCH, DELETE"
-  );
+//   // Request methods you wish to allow
+//   res.setHeader(
+//     "Access-Control-Allow-Methods",
+//     "GET, POST, PUT, PATCH, DELETE"
+//   );
 
-  // Request headers you wish to allow
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
-  );
+//   // Request headers you wish to allow
+//   res.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "X-Requested-With,content-type"
+//   );
 
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  // res.setHeader("Access-Control-Allow-Credentials", true);
+//   // Set to true if you need the website to include cookies in the requests sent
+//   // to the API (e.g. in case you use sessions)
+//   // res.setHeader("Access-Control-Allow-Credentials", true);
 
-  // Pass to next layer of middleware
-  next();
-});
+//   // Pass to next layer of middleware
+//   next();
+// });
 
 
 
@@ -73,5 +73,5 @@ const closeHandler = () => {
   });
 };
 
-// process.on("SIGTERM", closeHandler);
-// process.on("SIGINT", closeHandler);
+process.on("SIGTERM", closeHandler);
+process.on("SIGINT", closeHandler);
